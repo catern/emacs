bd9fcf59ff65cc0fce119d85303fecc69d5064b1
Spencer Baugh <sbaugh@janestreet.com>
2025-04-08 17:13:08 -0400

    Signal end-of-file with more correct data
    end_of_file_error previously always signaled end-of-file with
    load-true-file-name if that was non-nil (and a string).
    However, this might be the wrong thing to do; for example, if a
    file being loaded calls read on a buffer.
    
    We can determine what's correct by also checking if readcharfun
    is reading from a file.  Also, by checking that we can signal a
    better error for read on a buffer.
    
    * src/lread.c (end_of_file_error): Check readcharfun to
    determine what data to signal with.  (bug#68546)
    (read_char_escape, read_char_literal, read_string_literal)
    (skip_space_and_comments, read0): Pass readcharfun to
    end_of_file_error.

--- src/lread.c
+++ src/lread.c
@@ -2342,12 +2342,14 @@ readevalloop_1 (int old)
    information.  */
 
 static AVOID
-end_of_file_error (void)
+end_of_file_error (Lisp_Object readcharfun)
 {
-  if (STRINGP (Vload_true_file_name))
+  if (FROM_FILE_P (readcharfun) && STRINGP (Vload_true_file_name))
     xsignal1 (Qend_of_file, Vload_true_file_name);
-
-  xsignal0 (Qend_of_file);
+  else if (BUFFERP (readcharfun))
+    xsignal1 (Qend_of_file, readcharfun);
+  else
+    xsignal0 (Qend_of_file);
 }
 
 static Lisp_Object
@@ -2858,7 +2860,7 @@ read_char_escape (Lisp_Object readcharfun, int next_char)
   switch (c)
     {
     case -1:
-      end_of_file_error ();
+      end_of_file_error (readcharfun);
 
     case 'a': chr = '\a'; break;
     case 'b': chr = '\b'; break;
@@ -3031,7 +3033,7 @@ read_char_escape (Lisp_Object readcharfun, int next_char)
           {
             int c = READCHAR;
             if (c < 0)
-              end_of_file_error ();
+              end_of_file_error (readcharfun);
             if (c == '}')
               break;
             if (c >= 0x80)
@@ -3201,7 +3203,7 @@ read_char_literal (Lisp_Object readcharfun)
 {
   int ch = READCHAR;
   if (ch < 0)
-    end_of_file_error ();
+    end_of_file_error (readcharfun);
 
   /* Accept `single space' syntax like (list ? x) where the
      whitespace character is SPC or TAB.
@@ -3347,7 +3349,7 @@ read_string_literal (Lisp_Object readcharfun)
     }
 
   if (ch < 0)
-    end_of_file_error ();
+    end_of_file_error (readcharfun);
 
   if (!force_multibyte && force_singlebyte)
     {
@@ -3777,7 +3779,7 @@ skip_space_and_comments (Lisp_Object readcharfun)
 	  c = READCHAR;
 	while (c >= 0 && c != '\n');
       if (c < 0)
-	end_of_file_error ();
+	end_of_file_error (readcharfun);
     }
   while (c <= 32 || c == NO_BREAK_SPACE);
   UNREAD (c);
@@ -3972,7 +3974,7 @@ read0 (Lisp_Object readcharfun, bool locate_syms)
   bool multibyte;
   int c = READCHAR_REPORT_MULTIBYTE (&multibyte);
   if (c < 0)
-    end_of_file_error ();
+    end_of_file_error (readcharfun);
 
   switch (c)
     {
@@ -4403,7 +4405,7 @@ read0 (Lisp_Object readcharfun, bool locate_syms)
 	      {
 		c = READCHAR;
 		if (c < 0)
-		  end_of_file_error ();
+		  end_of_file_error (readcharfun);
 		quoted = true;
 	      }
 
